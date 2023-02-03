e952d754627b95c2dfd248bab027a474fc5a6b06
a-sepulveda-r <ariel.sepulvedar@gmail.com>
2023-02-02 23:38:20 -0300

    commit3

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -3,6 +3,8 @@ package com.bolsadeideas.springboot.backend.apirest.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,14 +13,20 @@ import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 
 @Service
 public class ClienteServiceImpl implements IClienteService {
-	
+
 	@Autowired
 	private IClienteDao clienteDao;
-	
+
 	@Override
 	@Transactional(readOnly = true)
 	public List<Cliente> findAll() {
-		return(List<Cliente>) clienteDao.findAll();
+		return (List<Cliente>) clienteDao.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Cliente> findAll(Pageable pageable) {
+		return clienteDao.findAll(pageable);
 	}
 
 	@Override
@@ -36,6 +44,6 @@ public class ClienteServiceImpl implements IClienteService {
 	@Override
 	@Transactional
 	public void delete(Long id) {
-		clienteDao.deleteById(id);		
+		clienteDao.deleteById(id);
 	}
 }
--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
@@ -2,16 +2,21 @@ package com.bolsadeideas.springboot.backend.apirest.models.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 
 // Aca se ponen los metodos del crud
 public interface IClienteService {
 	public List<Cliente> findAll();
 
+	public Page<Cliente> findAll(Pageable pageable);
+
 	public Cliente findById(Long id);
 
-	public Cliente save (Cliente cliente);
+	public Cliente save(Cliente cliente);
 
-	public void delete (Long id);
+	public void delete(Long id);
 
 }
