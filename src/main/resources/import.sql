727cd4befcd97c884036aad76edf42b19df0b107
a-sepulveda-r <ariel.sepulvedar@gmail.com>
2023-02-08 17:45:43 -0300

    commit2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,13 @@
 /* Populate tabla clientes */
 
-INSERT INTO regiones (id, nombre) VALUES (1, 'Sudamérica');
-INSERT INTO regiones (id, nombre) VALUES (2, 'Centroamérica');
-INSERT INTO regiones (id, nombre) VALUES (3, 'Norteamérica');
+INSERT INTO regiones (id, nombre) VALUES (1, 'Sudamerica');
+INSERT INTO regiones (id, nombre) VALUES (2, 'Centroamerica');
+INSERT INTO regiones (id, nombre) VALUES (3, 'Norteamerica');
 INSERT INTO regiones (id, nombre) VALUES (4, 'Europa');
 INSERT INTO regiones (id, nombre) VALUES (5, 'Asia');
 INSERT INTO regiones (id, nombre) VALUES (6, 'Africa');
-INSERT INTO regiones (id, nombre) VALUES (7, 'Oceanía');
-INSERT INTO regiones (id, nombre) VALUES (8, 'Antártida');
+INSERT INTO regiones (id, nombre) VALUES (7, 'Oceania');
+INSERT INTO regiones (id, nombre) VALUES (8, 'Antartida');
 
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(1, 'Andrés', 'Guzmán', 'profesor@bolsadeideas.com', '2018-01-01');
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(2, 'Mr. John', 'Doe', 'john.doe@gmail.com', '2018-01-02');
