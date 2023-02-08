da2e85682443e74338ef9d38faf1d42670e6e42a
a-sepulveda-r <ariel.sepulvedar@gmail.com>
2023-02-08 17:01:52 -0300

    commit2

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IClienteDao.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -1,11 +1,18 @@
 package com.bolsadeideas.springboot.backend.apirest.models.dao;
 
+import java.util.List;
+
 // import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Region;
 
 //en esta clase DAO se implemtan los crud de forma automatica
 public interface IClienteDao extends JpaRepository<Cliente, Long> {
+
+    @Query("from Region")
+    public List<Region> findAllRegiones();
 
 }
--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -3,11 +3,16 @@ package com.bolsadeideas.springboot.backend.apirest.models.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 //import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
@@ -44,6 +49,11 @@ public class Cliente implements Serializable {
     private Date createAt;
 
     private String foto;
+    @NotNull(message = "no puede estar vacío")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+    private Region region;
 
     // @PrePersist
     // public void prePersist() {
@@ -96,6 +106,14 @@ public class Cliente implements Serializable {
 
     public void setCreateAt(Date createAt) {
         this.createAt = createAt;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
     }
 
     private static final long serialVersionUID = 1L;
